bff93bc5aa57a2eb99c6e89ab651bf490792883a
kamir222 <katia.amir@gmail.com>
2017-06-06 22:34:17 +0000

    added middleware body-parser; new routing request

--- express_server.js
+++ express_server.js
@@ -1,15 +1,16 @@
 "use strict";
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const PORT  = process.env.PORT || 8080; //if there is preconfigured port choose that, otherwise choose 8080
 
 app.set('view engine', 'ejs') // tells express to use ejs as templating engine
+app.use(bodyParser.urlencoded({extended: true})); // turning data into object (attaches form data to req.body)
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 
 app.get('/', (req, res) => {
   res.end('Hello!');
@@ -25,13 +26,34 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+  console.log("/urls/new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 app.get('/urls/:id', (req, res) => {
+  //console.log('/urls/:id', req.params.id); //checking which endpoint it hits first
   let urlId = req.params.id;
   let fullUrl = urlDatabase[urlId];
   let templateVars = { shortURL: urlId, longURL: fullUrl };
   res.render("urls_show", templateVars);
 })
 
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+function generateRandomString() {
+  var randomString = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < 6; i++ )
+      randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return randomString;
+}
