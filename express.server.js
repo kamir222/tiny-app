bfafaf1713303981232c8c855d423eb2f902ab1b
kamir222 <katia.amir@gmail.com>
2017-06-06 20:13:40 +0000

    use ejs with express

--- express.server.js
+++ express.server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const PORT  = process.env.PORT || 8080; //if there is preconfigured port choose that, otherwise choose 8080
 
+app.set('view engine', 'ejs') // tells express to use ejs as templating engine
+
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get('/', (req, res) => {
   res.end('Hello!');
